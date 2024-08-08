82c148525eb50846104b5f01bad1facd9db09146
Bhanu Prakash Paila <pailabhanuprakash426@gmail.com>
2024-08-08 15:33:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+"use strict";
+
+console.log(document.querySelector('.message').textContent)
+
+document.querySelector('.message').textContent='Start Guessing !';
+// document.querySelector('.btn again').textContent='repeat';
+document.querySelector('.score').textContent=20;
+
+document.querySelector('.guess').value=0;
+console.log(document.querySelector('.guess').value);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0;
+const viewMessage=function (message){
+    document.querySelector('.message').textContent=message;
+}
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    // document.querySelector(".message").textContent = "no number";
+    viewMessage("no number");
+  } else if(guess!=secretNumber){
+    if (score > 1) {
+        document.querySelector(".message").textContent = guess>secretNumber? "too high !":"too low !";
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+        // document.querySelector(".message").textContent = "you lost the game ";
+        viewMessage("you lost the game💣")
+        document.querySelector(".score").textContent = 0;
+      }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "you lost the game ";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "you lost the game ";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess === secretNumber) {
+    // document.querySelector(".message").textContent = "correct number";
+    viewMessage("correct number");
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b437";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+    score=20
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    // document.querySelector(".message").textContent = "Start Guessing";
+    viewMessage("Start Guessing...")
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?";
+    document.querySelector('.guess').value='';
+    document.querySelector("body").style.backgroundColor = "rgb(255,255,255)";
+    document.querySelector('.highscore').textContent = highscore;
+});
